642b74cbfe1e1d55e13102b83ac752a44a4ebd76
rxm562 <51394123+rxm562@users.noreply.github.com>
2020-07-06 02:32:42 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,71 +1,47 @@
-"""
-This file to define Fragility Curves
-"""
-
-import numpy as np
+# ALA (2001) Correction Modified Calculator
 import pandas as pd
-from scipy.spatial import distance
-import matplotlib.pyplot as plt
-import networkx as nx
 
-def plot_fragility_curve(FC, fill=True, key='Default', 
-                         title='', xmin=0, xmax=250, npoints=100, 
-                         xlabel='x', 
-                         ylabel='Probability of exceedance',
-                         figsize=[10,6]):
-    """
-    Plot fragility curve.
-    
-    Parameters
-    -----------
-    FC : FragilityCurve object
-        Fragility curve
-    
-    fill : bool (optional)
-        If true, fill area under the curve (default = True)
-    
-    key : string (optional)
-        Fragility curve state distribution key (default = 'Default')
-    
-    title : string (optional)
-        Plot title
-    
-    xmin : float (optional)
-        X axis minimum (default = 0)
-    
-    xmax : float (optional)
-        X axis maximum (default = 1)
-    
-    npoints : int (optional)
-        Number of points (default = 100)
-    
-    xlabel : string (optional)
-        X axis label (default = 'x')
-    
-    ylabel : string (optional)
-        Y axis label (default = 'Probability of exceedance')
-    
-    figsize : list (optional)
-        Figure size (default = [10,5])
-"""
-    if plt is None:
-        raise ImportError('matplotlib is required')
-    
-    plt.figure(figsize=tuple(figsize))
-    plt.title(title)
-    x = np.linspace(xmin,xmax,npoints)
-    for name, state in FC.states():
-        try:
-            dist=state.distribution[key]
-            if fill:
-                plt.fill_between(x,dist.cdf(x), label=name)
+
+# calculator
+def Correction_Modifiers(pipe_info, Mat_type=None, Soil_type=None, Age=None):
+    pipe_info = pd.DataFrame(data = pipe_info.values, columns =pipe_info.columns, index = pipe_info.index.astype('str')) # Pipe characteristics, strings
+                  
+    if Mat_type is None:
+        k1_wei = {'AC': 1.0, 'CI': 1.0, 'DI': 0.5, 'PVC': 0.5, 'STL': 0.7, 'RCCP': 0.2}
+        
+    def corrosion_factor(arrlike):
+        Age = arrlike['Age']
+        Soil_type = arrlike['Soil_type']
+        Mat_type = arrlike['Mat_type']
+        if Mat_type == 'CI':
+            if Soil_type == 'H':
+                if Age < 1920:
+                    corrosion_rate = 3.0
+                elif Age < 1960:
+                    corrosion_rate = -0.05*Age+99
+                else:
+                    corrosion_rate = 1.0
+            elif Soil_type == 'M':
+                if Age < 1920:
+                    corrosion_rate = 2.0
+                elif Age < 1960:
+                    corrosion_rate = -0.025*Age+50
+                else:
+                    corrosion_rate = 1.0
+            elif Soil_type == 'L':
+                corrosion_rate = 1.0
             else:
-                plt.plot(x,dist.cdf(x), label=name)
-        except:
-            pass        
-    plt.xlim((xmin,xmax))
-    plt.ylim((0,1))
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.legend()
+                print('Soil Type Index Wrong')
 
+        elif Mat_type == 'DI':
+            corrosion_rate = 1.5
+        else:
+            corrosion_rate = 1.0
+            
+        return corrosion_rate
+        
+    k1 = pipe_info['Mat_type'].map(k1_wei)            # Modifier for material type
+    k2 = pipe_info.apply(corrosion_factor,axis = 1)   # Modifier for corrosion
+    C = k1*k2
+
+    return C
